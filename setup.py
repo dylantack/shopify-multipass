3685c794212b8e44f99a311708421d713b47dd46
Dylan Tack <dylan.tack@ui.com>
2021-01-26 15:48:42 -0800

    rename due to conflict with unpublish package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='shopify-multipass',
+    name='shopify-multipass-login',
     version='2.0.0',
     description='Shopify Multipass module for Python',
     url='https://github.com/dylantack/shopify-multipass',
